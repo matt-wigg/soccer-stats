c2478856637aececd094929c2af870b3970041a0
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-09-15 10:52:40 -0700

    Removes unused webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -23,17 +23,6 @@ module.exports = {
           },
         },
       },
-      {
-        test: /\.ttf$/,
-        use: [
-          {
-            loader: 'ttf-loader',
-            options: {
-              name: './font/[hash].[ext]',
-            },
-          },
-        ],
-      },
     ],
   },
   resolve: {
